1e2e6b01360d47196257ec053d6252b497dcd732
yu5github <yu_w_main@yahoo.co.jp>
2023-02-15 18:42:48 +0900

    Charccterクラスの基本機能の追加

---
+++ RPG/src/framework/Character.java
@@ -0,0 +1,32 @@
+package framework;
+
+import java.util.Objects;
+
+public abstract class Character {
+	protected int hp;
+
+	int getHp() {
+		return hp;
+	}
+
+	void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hp);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Character other = (Character) obj;
+		return hp == other.hp;
+	}
+}
